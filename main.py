2bdb9a38713465a4d0e8a28fd7ff07e62a8b36bd
Nash Adam Munoz <150409949+lichclass@users.noreply.github.com>
2025-11-17 20:51:07 +0800

    feat: added the dataset

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ parser = argparse.ArgumentParser(
     description='Federated Prototypical Network with Residual Networks and Atrous Spatial Pyramid Pooling for SeaTurtleID2022'
 )
 
+# Inspect the dataset annotations
 parser.add_argument(
     "-ia", "--inspect-annotations",
     action="store_true",
     help="Inspect the dataset annotations"
+)
+
+# Small Easter Egg
+parser.add_argument(
+    "-mes", "--message",
+    action="store_true",
+    help="Message from the developers"
+)
+
+# Display the overview of the Thesis Project
+parser.add_argument(
+    "-ov", "--overview",
+    action="store_true",
+    help="Display Project Overview"
 )
 
 if __name__ == "__main__":
@@ -16,3 +31,17 @@ if __name__ == "__main__":
 
     if args.inspect_annotations:
         inspect_annotations()
+
+    if args.message:
+        developer_msg = "Nice to meet you! We are Nash Adam Muñoz and Elijah Kahlil Andres Abangan. Thank you for stopping by our Thesis Project!"
+        print("="*len(developer_msg))
+        print("Message from the Developers: \n")
+        print(developer_msg)
+        print("="*len(developer_msg))
+
+    if args.overview:
+        overview_msg = "Federated Prototypical Network with Residual Networks and Atrous Spatial Pyramid Pooling for SeaTurtleID2022"
+        print("="*len(overview_msg))
+        print("Project Overview: \n")
+        print(overview_msg)
+        print("="*len(overview_msg))
